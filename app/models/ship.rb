8c62a318718e54b845c030f024268261068ce5dd
bfosberry <brendan@codeship.com>
2015-09-21 15:48:37 -0500

    Updating ship to create an identity token

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -16,7 +16,7 @@ class Ship < ActiveRecord::Base
   end
 
   def self.default_attributes
-    { shield: 100, energy: 100, source: "127.0.0.1" }
+    { shield: 100, energy: 100, token: SecureRandom.uuid }
   end
 
   private
