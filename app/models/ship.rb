b9b1668169c329db59c68a881499f66537698ea3
bfosberry <brendan@codeship.com>
2015-09-21 23:19:03 -0500

    Adding default sector

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -77,7 +77,7 @@ class Ship < ActiveRecord::Base
   end
 
   def self.default_attributes
-    { shield: 100, energy: 100, token: SecureRandom.uuid, last_charged_at: DateTime.now }
+    { shield: 100, energy: 100, token: SecureRandom.uuid, last_charged_at: DateTime.now, sector: Sector.first }
   end
 
   def spawn
