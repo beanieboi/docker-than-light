56174eaa52d6521752c2b28887457780a60cbb90
bfosberry <brendan@codeship.com>
2015-11-26 20:12:49 -0600

    Adding support for swarm by allowing tls loading from env

---
+++ config/initializers/docker.rb
@@ -0,0 +1,22 @@
+require 'tempfile'
+tmp_dir = "#{Rails.root}/tmp"
+cert_file = Tempfile.new "docker_cert", tmp_dir 
+cert_file.write(ENV["DOCKER_CERT"])
+cert_file.close
+
+key_file = Tempfile.new "docker_key", tmp_dir 
+key_file.write(ENV["DOCKER_KEY"])
+key_file.close
+
+ca_file = Tempfile.new "docker_ca", tmp_dir 
+ca_file.write(ENV["DOCKER_CA"])
+ca_file.close
+
+DOCKER_KEYS=[ca_file, key_file, cert_file]
+
+Docker.options = {
+    client_cert: cert_file.path,
+    client_key: key_file.path,
+    ssl_ca_file: ca_file.path,
+    scheme: 'https'
+}
