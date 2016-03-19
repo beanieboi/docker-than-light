61b61b50225caedd5d53f770820a1d3c1cdf389b
beanieboi <beanie@benle.de>
2016-03-19 14:18:06 +0100

    update api style get_ship -> ship and new hash syntax

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -13,12 +13,13 @@ class Api::ApiController < Api::ApplicationController
   def fire
     @ship = ship
     @ship_to_shoot = Ship.find_by_name(params[:name])
+
     if @ship.fire!(@ship_to_shoot)
       render json: {
         state: @ship,
       }
     else
-      render nothing: true, :status => 400
+      render nothing: true, status: 400
     end
   end
 
@@ -52,17 +53,17 @@ class Api::ApiController < Api::ApplicationController
   end
 
   def travel
-    @ship = get_ship
+    @ship = ship
     sector = Sector.find_by_name(params[:sector])
     unless sector
-      render :nothing => true, :status => 404
+      render nothing: true, status: 404
     end
     if @ship.travel!(sector)
       render :json => {
         :state => @ship,
       }
     else
-      render :nothing => true, :status => 400
+      render nothing: true, status: 400
     end
   end
 
