0c4e5c04db41bc05f26721abe28fbf1a6637b4cd
Brendan Fosberry <brendan.fosberry@gmail.com>
2016-03-22 21:14:23 -0500

    Supporting API tokens for client auth.
    This change splits out the api controller from the regular
    application controller base. With this change incoming api requests
    are verified based on an api token, and also the source IP

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,10 +1,22 @@
-class Api::ApplicationController < ApplicationController
+class Api::ApplicationController < ActionController::Base
 	skip_before_filter :verify_authenticity_token
   protect_from_forgery with: :null_session
 
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
-
+  before_action :authenticate
   private
+
+  def authenticate
+    if authentication_token && ship
+      unless ship.source.include? request.remote_ip
+        render nothing: true, status: 404
+      end
+    end
+  end
+
+  def ship
+    Ship.find_by!(token: authentication_token)
+  end
 
   def authentication_token
   	unless request.headers.include?('Authorization')
